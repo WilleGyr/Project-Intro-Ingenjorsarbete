0708c1ff946d24abf3cc5957f762e91f4bddc1f4
William Gyrulf <37251199+WilleGyr@users.noreply.github.com>
2023-10-31 17:24:08 +0100

    Update MapLoader.py

--- Kod/MapLoader.py
+++ Kod/MapLoader.py
@@ -6,16 +6,19 @@ pygame.init()
 screen_width = 1280
 screen_height = 720
 
+# Sets the screen resolution
 screen = pygame.display.set_mode((screen_width, screen_height))
+
+# Sets the name of the window to "Platformer"
 pygame.display.set_caption('Platformer')
 
-#define game variables
 tile_size = 20
 
 class World():
 	def __init__(self, data):
 		self.tile_list = []
 
+		# Imports selected images
 		dirt_img = pygame.image.load('Data_files/images/tiles/grass/5.png')
 		grass_img = pygame.image.load('Data_files/images/tiles/grass/1.png')
 		grass_r_img = pygame.image.load('Data_files/images/tiles/grass/2.png')
@@ -36,16 +39,32 @@ class World():
 		
 
 		row_count = 0
+		
+		# Goes through every row
 		for row in data:
 			col_count = 0
+			# Goes through every column
 			for tile in row:
 				if tile == 1:
+					# Scale the image to the right size
 					img = pygame.transform.scale(border_img, (tile_size, tile_size))
+					
+					# Creates a rectangle to represent the image's position and dimensions
 					img_rect = img.get_rect()
+					
+					# The x position of the tile
 					img_rect.x = col_count * tile_size
+					
+					# The y position of the tile
 					img_rect.y = row_count * tile_size
+					
+					# Creates a tile with the image and its position
 					tile = (img, img_rect)
+					
+					# Adds the tile to the tile list
 					self.tile_list.append(tile)
+					
+					# Repeats for every image
 				if tile == 2:
 					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
 					img_rect = img.get_rect()
@@ -162,12 +181,14 @@ class World():
 			row_count += 1
 
 	def draw(self):
+		# Goes through every tile in the tile list
 		for tile in self.tile_list:
+			# Renders the tile at its specified position
 			screen.blit(tile[0], tile[1])
 
 
-
-world_data = [
+# The tilemap for the test map
+testmap = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -206,6 +227,7 @@ world_data = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+# Tilemap for the first level
 level1 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -245,6 +267,7 @@ level1 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+# Tilemap for the second level
 level2 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -284,6 +307,7 @@ level2 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+# Tilemap for the third level
 level3 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 	[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -323,21 +347,32 @@ level3 = [
 	[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
+# Imports the background image
 bg = pygame.image.load("Data_files/images/himmel.png")
 
-world = World(world_data)
+# Creates a world objects of every tilemap
+world = World(testmap)
 world_level1 = World(level1)
 world_level2 = World(level2)
 world_level3 = World(level3)
+
+# Sets the chosen world to the test map for now
 selectedWorld = world
 
 run = True
 while run:
+	# Sets the bakground image
 	screen.blit(bg, (0, 0))
+	
+	# Renders the selected world
 	selectedWorld.draw()
 	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
+
+		# Exits the application if the window close button is pressed
+		if event.type == pygame.QUIT:w
 			run = False
+			
+		# Changes the map between level 1, 2, 3 and 4  (test map)
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_1:
 				selectedWorld = world_level1
