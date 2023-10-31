e985ac75d1f6dd7a5c9f81248b54f5931689c5d3
JakubHacz <98486254+Potatoman54@users.noreply.github.com>
2023-10-31 10:38:18 +0100

    revert if not work

--- Kod/main.py
+++ Kod/main.py
@@ -1,11 +1,32 @@
+import sys
 import json
 import pygame
 
-def load(self, path):
-    f = open(path, 'r')
-    map_data = json.load(f)
-    f.close()
-    
-    self.tilemap = map_data['tilemap']
-    self.tile_size = map_data['tile_size']
-    self.offgrid_tiles = map_data['offgrid']
+class Game:
+    def __init__(self, path):
+        pygame.init()
+
+        pygame.display.set_caption('test')
+        self.screen = pygame.display.set_mode((640, 480))
+
+        self.clock = pygame.time.Clock()
+
+        f = open(path, 'r')
+        map_data = json.load(f)
+        f.close()
+        
+        self.tilemap = map_data['tilemap']
+        self.tile_size = map_data['tile_size']
+        self.offgrid_tiles = map_data['offgrid']
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
