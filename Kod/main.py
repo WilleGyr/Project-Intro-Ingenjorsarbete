5e2d00ac456633c473f2491e6e45857ff969b7bd
William Gyrulf <37251199+WilleGyr@users.noreply.github.com>
2023-10-31 10:32:01 +0100

    Update main.py

--- Kod/main.py
+++ Kod/main.py
@@ -1,1 +1,11 @@
+import json
+import pygame
 
+def load(self, path):
+    f = open(path, 'r')
+    map_data = json.load(f)
+    f.close()
+    
+    self.tilemap = map_data['tilemap']
+    self.tile_size = map_data['tile_size']
+    self.offgrid_tiles = map_data['offgrid']
