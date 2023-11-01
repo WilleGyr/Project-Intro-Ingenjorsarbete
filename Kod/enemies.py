bba8ae4ec2d10cd0ac1513372f62618f8866664d
JakubHacz <98486254+Potatoman54@users.noreply.github.com>
2023-11-01 17:39:59 +0100

    Update enemies.py

--- Kod/enemies.py
+++ Kod/enemies.py
@@ -1,5 +1,20 @@
 import pygame
 
-class Enemies(pygame.sprite.Sprite):
-    def __init__(self):
-        pass
+class Enemy:
+    def __init__(self, game, e_type, pos, size, speed, shoots, spawns):
+        self.game = game
+        self.type = e_type
+        self.pos = list(pos)
+        self.size = size
+        self.speed = speed
+        self.shoots = shoots
+        self.spawns = spawns
+
+    def render(self, surf):
+        if self.type == 1:
+            pygame.draw.circle(surf, (0, 0, 255), (250, 250), 75)
+        elif self.type == 2:
+            pass
+        else:
+            pass
+        surf.blit(self.game.assets["player"], self.pos)
