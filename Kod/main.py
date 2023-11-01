7c082f66b07c788a27952d8c0d1e9de6bb628715
JakubHacz <98486254+Potatoman54@users.noreply.github.com>
2023-11-01 18:41:59 +0100

    added comments to main.py

--- Kod/main.py
+++ Kod/main.py
@@ -4,55 +4,53 @@ import pygame
 from pygame.locals import *
 from enemies import Enemy
 from MapLoader import World
+# Imports modules and classes from other files
 
 class Game:
     def __init__(self):
-        pygame.init()
-        pygame.display.set_caption("Not Mario")
+        pygame.init()                           # Initialises pygame
+        pygame.display.set_caption("Not Mario") # Sets display name 
 
-        SCREEN_WIDTH = 1280
-        SCREEN_HEIGHT = 720
+        SCREEN_WIDTH = 1280 #
+        SCREEN_HEIGHT = 720 # Sets constants
 
-        bg = pygame.image.load("Data_files/images/himmel.png")
+        bg = pygame.image.load("Data_files/images/himmel.png") # Sets background
 
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Sets window size
 
-        self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock() # Sets clock to regulate frames per second
         
-        with open ("tilemaps/testmap.json", "r") as file4:
+        with open ("tilemaps/testmap.json", "r") as file4: 
             testmap = json.load(file4)
                     
-        with open ("tilemaps/level1.json", "r") as file1:
+        with open ("tilemaps/level1.json", "r") as file1:  
             level1 = json.load(file1)
                     
         with open ("tilemaps/level2.json", "r") as file2:
             level2 = json.load(file2)
 
-        with open ("tilemaps/level3.json", "r") as file3:
+        with open ("tilemaps/level3.json", "r") as file3: # Imports the world maps - consider moving to maploader.py?
             level3 = json.load(file3)
 
-        world = World(testmap)
-        world_level1 = World(level1)
-        world_level2 = World(level2)
-        world_level3 = World(level3)
+        world = World(testmap)       #
+        world_level1 = World(level1) #
+        world_level2 = World(level2) #
+        world_level3 = World(level3) # Sets worlds as variables using World() class 
 
-        selectedWorld = world
+        selectedWorld = world # Selects starting world
 
         while True:
-            # Sets the bakground image
-            self.screen.blit(bg, (0, 0))
+            self.screen.blit(bg, (0, 0)) # Sets the bakground image
             
-            # Renders the selected world
-            selectedWorld.draw(self)
+            selectedWorld.draw(self) # Renders the selected world
 
-            Enemy(1, (50, 50), (8, 15), 5, False, False).render(self.screen)
-            Enemy(2, (50, 50), (8, 15), 5, False, False).render(self.screen)
-            Enemy(3, (50, 50), (8, 15), 5, False, False).render(self.screen)
+            Enemy(1, (50, 50), (8, 15), 5, False, False).render(self.screen) #
+            Enemy(2, (50, 50), (8, 15), 5, False, False).render(self.screen) #  
+            Enemy(3, (50, 50), (8, 15), 5, False, False).render(self.screen) # Test render for enemy.py functionality
 
             for event in pygame.event.get():
 
-                # Exits the application if the window close button is pressed
-                if event.type == pygame.QUIT:
+                if event.type == pygame.QUIT: # Exits the application if the window close button is pressed
                     pygame.quit()
                     sys.exit()
                     
