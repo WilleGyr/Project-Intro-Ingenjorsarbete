1c2ae5c3bf924cbf3e79a3715c08d773896cdd20
JakubHacz <98486254+Potatoman54@users.noreply.github.com>
2023-11-04 16:21:13 +0100

    Update enemies.py

--- Kod/enemies.py
+++ Kod/enemies.py
@@ -7,28 +7,45 @@ class Enemy(pygame.sprite.Sprite):
         self.rev_enemy_img = pygame.transform.flip(pygame.image.load('Data_files/images/entities/enemy/idle/00.png'), True, False)
 
 
-    def render_enemy(self, surf, e_type, loc):
+    def render_enemy(self, surf, e_type, loc, dir):
         #GROUND_LEVEL = 540
 
         if e_type == 1:
             img = pygame.transform.scale(self.enemy_img, (6*6, 4*15)) # Image + size
             rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
-            pos = (loc[0], loc[1] - 4*15) # Location
-            surf.blit(img, pos)
+            pos = [loc[0], loc[1] - 4*15] # Location
+
+            if dir:
+                surf.blit(img, pos)
+            else:
+                surf.blit(rev_img, pos)
             
         elif e_type == 2:
             img = pygame.transform.scale(self.enemy_img, (8*6, 5*15))
-            rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
+            rev_img = pygame.transform.scale(self.rev_enemy_img, (8*6, 5*15)) # Image + size
             pos = (loc[0], loc[1] - 5*15)
             surf.blit(img, pos)
+
+            if dir:
+                surf.blit(img, pos)
+            else:
+                surf.blit(rev_img, pos)
+
         else:
             img = pygame.transform.scale(self.enemy_img, (14*6, 8*15))
-            rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
+            rev_img = pygame.transform.scale(self.rev_enemy_img, (14*6, 8*15)) # Image + size
             pos = (loc[0], loc[1] - 8*15)
             surf.blit(img, pos)
 
-    def render_2(self,surf):
-        self.render_enemy(surf, 2, (900, 480))
+            if dir:
+                surf.blit(img, pos)
+            else:
+                surf.blit(rev_img, pos)
 
-    def render_3(self,surf):
-        self.render_enemy(surf, 3, (1000, 440))
+    def get_hitbox(self, e_type, pos):
+        if e_type == 1:
+            return pygame.Rect((pos[0], pos[1]), (6*6, 4*15))
+        elif e_type == 2:
+            return pygame.Rect((pos[0], pos[1]), (8*6, 5*15))
+        else:
+            return pygame.Rect((pos[0], pos[1]), (14*6, 8*15))
