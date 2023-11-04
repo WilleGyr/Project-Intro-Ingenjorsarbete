9fce25394bb5d77355e27e6108097ac3ef03fa0f
JakubHacz <98486254+Potatoman54@users.noreply.github.com>
2023-11-04 15:35:51 +0100

    Update enemies.py

--- Kod/enemies.py
+++ Kod/enemies.py
@@ -1,18 +1,34 @@
 import pygame
 
-class Enemy:
-    def __init__(self, e_type, pos, size, speed, shoots, spawns):
-        self.type = e_type
-        self.pos = list(pos)
-        self.size = size
-        self.speed = speed
-        self.shoots = shoots
-        self.spawns = spawns
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
 
-    def render(self, surf):
-        if self.type == 1:
-            pygame.draw.circle(surf, (0, 0, 255), (250, 250), 75)
-        elif self.type == 2:
-            pygame.draw.circle(surf, (0, 255, 0), (500, 250), 75)
+        self.enemy_img = pygame.image.load('Data_files/images/entities/enemy/idle/00.png')
+        self.rev_enemy_img = pygame.transform.flip(pygame.image.load('Data_files/images/entities/enemy/idle/00.png'), True, False)
+
+
+    def render_enemy(self, surf, e_type, loc):
+        #GROUND_LEVEL = 540
+
+        if e_type == 1:
+            img = pygame.transform.scale(self.enemy_img, (6*6, 4*15)) # Image + size
+            rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
+            pos = (loc[0], loc[1] - 4*15) # Location
+            surf.blit(img, pos)
+            
+        elif e_type == 2:
+            img = pygame.transform.scale(self.enemy_img, (8*6, 5*15))
+            rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
+            pos = (loc[0], loc[1] - 5*15)
+            surf.blit(img, pos)
         else:
-            pygame.draw.circle(surf, (255, 0, 0), (750, 250), 75)
+            img = pygame.transform.scale(self.enemy_img, (14*6, 8*15))
+            rev_img = pygame.transform.scale(self.rev_enemy_img, (6*6, 4*15)) # Image + size
+            pos = (loc[0], loc[1] - 8*15)
+            surf.blit(img, pos)
+
+    def render_2(self,surf):
+        self.render_enemy(surf, 2, (900, 480))
+
+    def render_3(self,surf):
+        self.render_enemy(surf, 3, (1000, 440))
